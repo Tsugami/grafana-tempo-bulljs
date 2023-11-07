c6a680c2dd60ca372f93fd2b869a81c317aa928e
Yslan Ramos <yslanramos.r@gmail.com>
2023-11-07 11:03:55 -0300

    remove unused code on instrumentation.js

--- backend/instrumentation.js
+++ backend/instrumentation.js
@@ -1,37 +1,23 @@
 require("dotenv/config");
 
-/*instrumentation.js*/
-// Require dependencies
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const {
   getNodeAutoInstrumentations,
 } = require("@opentelemetry/auto-instrumentations-node");
-const {
-  PeriodicExportingMetricReader,
-  // ConsoleMetricExporter,
-} = require("@opentelemetry/sdk-metrics");
+const { PeriodicExportingMetricReader } = require("@opentelemetry/sdk-metrics");
 const {
   BullMQInstrumentation,
 } = require("@jenniferplusplus/opentelemetry-instrumentation-bullmq");
 const {
   OTLPTraceExporter,
 } = require("@opentelemetry/exporter-trace-otlp-grpc");
-const {
-  OTLPTraceExporter: HttpOTLPTraceExporter,
-} = require("@opentelemetry/exporter-trace-otlp-http");
+
 const {
   OTLPMetricExporter,
-} = require("@opentelemetry/exporter-metrics-otlp-grpc");
-
-const {
-  OTLPMetricExporter: HttpOTLPMetricExporter,
 } = require("@opentelemetry/exporter-metrics-otlp-http");
-// const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
 
 const sdk = new NodeSDK({
-  traceExporter: new OTLPTraceExporter({
-    // url: "http://localhost:4318/v1/traces",
-  }),
+  traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
   }),
